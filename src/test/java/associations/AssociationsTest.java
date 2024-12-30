44d9219aa877b1bf21eee4fcef56e466ef53838e
coderHuahua <86610939+coderHuahua@users.noreply.github.com>
2024-12-30 15:29:10 -0500

    Update AssociationsTest.java

--- src/test/java/associations/AssociationsTest.java
+++ src/test/java/associations/AssociationsTest.java
@@ -90,10 +90,10 @@ class AssociationsTest {
     Customer c1 = new Customer("Weihua", "Liu");
 
     // Instantiate an Order
-    Order o1 = new Order("Dine-in","KI908B", LocalDateTime.now(),c1);
+    Order o1 = new Order("DineIn","KI908B", LocalDateTime.now(),c1);
 
     // Create an Item
-    Item item1 = new Item("Bergers", o1,"Spicy Crispy",2);
+    Item item1 = new Item("Burger", o1,"Spicy Crispy",2);
 
     // Create an Ingredient
     Ingredient ingre1 = new Ingredient(item1, "Potato Roll", "regular", 100.00);
@@ -117,10 +117,10 @@ class AssociationsTest {
     Customer c1 = new Customer("Weihua", "Liu");
 
     // Instantiate an Order
-    Order o1 = new Order("Dine-in","KI908B", LocalDateTime.now(),c1);
+    Order o1 = new Order("DineIn","KI908B", LocalDateTime.now(),c1);
 
     // Create an Item
-    Item item1 = new Item("Bergers", o1,"Spicy Crispy",2);
+    Item item1 = new Item("Burger", o1,"Spicy Crispy",2);
 
     // Create an Ingredient
     Ingredient ingre1 = new Ingredient(item1, "Potato Roll", "regular", 100.00);
